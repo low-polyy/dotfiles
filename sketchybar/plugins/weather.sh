5ab551343af945bc9ba287f26372e10490e97742
max <96744008+ura0aka@users.noreply.github.com>
2022-08-02 23:48:26 -0400

    Update weather.sh

--- sketchybar/plugins/weather.sh
+++ sketchybar/plugins/weather.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# this is made to work with the OpenWeatherMap.org API
 API_KEY="9c8508bea4766dc4c5a431947d3cbc51" # insert api key here
 CITY="Montreal" # insert city here
 LAT="45.501690" # insert latitude here
@@ -153,7 +154,6 @@ hour=$(date +%H)
 [ "$hour" -lt 19 -a "$hour" -ge 6 ] && icon=$weather_icons_day[$condition] || icon=$weather_icons_night[$condition]
 
 
-# [ "$is_day" > "1659260216" && "$is_day" < "1560396563" ] && icon=$weather_icons_day[$condition] || icon=$weather_icons_night[$condition]
 
 sketchybar -m \
     --set weather \
