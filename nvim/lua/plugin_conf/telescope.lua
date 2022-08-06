6f32a5dab2f0c121d87bdd2df29ad5382fcbc11f
Max Goussev <ura0aka@aka-sys.local>
2022-08-05 23:16:26 -0400

    updated telescope

--- nvim/lua/plugin_conf/telescope.lua
+++ nvim/lua/plugin_conf/telescope.lua
@@ -1,30 +1,31 @@
-require('telescope').setup{
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
-      }
-    }
-  },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  }
-}
+local actions = require('telescope.actions')
+
+require('telescope').setup({
+	defaults = {
+		file_sorter = require('telescope.sorters').get_fzy_sorter,
+		prompt_prefix = '  ',
+		color_devicons = true,
+
+		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
+		mappings = {
+			i = {
+				['<C-x>'] = false,
+				['<C-q>'] = actions.send_to_qflist,
+			},
+		},
+	},
+	-- extensions = {
+	-- 	fzy = {
+	--             fuzzy = true,
+	-- 		override_generic_sorter = false,
+	-- 		override_file_sorter = true,
+	--             case_mode = 'smart_case',
+	-- 	},
+	-- },
+})
+
+-- require('telescope').load_extension('fzf')
+
